e2cf371db542b37353ca70c782fca48f6debcc6e
Yasura Lakshitha <lakshitha200087@gmail.com>
2024-07-30 10:52:54 +0530

    Initial Commit

---
+++ src/test/java/edu/icet/hrms/EmployeeManagementSystemBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package edu.icet.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //TODO:
+    }
+
+}
